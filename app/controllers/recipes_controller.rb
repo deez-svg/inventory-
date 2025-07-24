6cce3455df8d904d8bb2383185a0f3353f1cd6c9
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2025-07-24 21:14:50 +0530

    recipe index

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,8 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.includes(ingredients: :material).all
+  end
+
   def show
     @recipe = Recipe.includes(ingredients: :material).find_by(id: params[:id])
   end
